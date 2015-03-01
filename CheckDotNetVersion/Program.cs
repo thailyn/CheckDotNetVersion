b03a1f735b988ed790af0a4ce8fb167067e810cd
Charles Macanka <cmacanka@gmail.com>
2015-02-28 20:26:22 -0500

    Get and print info about all installed versions.
    Code largely taken from http://stackoverflow.com/a/15926268 .  Modified
    to print out all values of the registry key, and fit my C# style.

--- CheckDotNetVersion/Program.cs
+++ CheckDotNetVersion/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,40 @@ namespace CheckDotNetVersion
     {
         static void Main(string[] args)
         {
+            string path = @"SOFTWARE\Microsoft\NET Framework Setup\NDP";
+            List<string> displayFrameworkName = new List<string>();
+
+            RegistryKey installedVersions = Registry.LocalMachine.OpenSubKey(path);
+            string[] versionNames = installedVersions.GetSubKeyNames();
+
+            for (int i = 1; i <= versionNames.Length - 1; i++)
+            {
+                string versionName = versionNames[i].ToString();
+                RegistryKey version = installedVersions.OpenSubKey(versionNames[i]);
+                System.Console.WriteLine("Microsoft .NET Framework " + versionName);
+
+                System.Console.WriteLine("Key Name: " + version.Name);
+
+                string[] valueNames = version.GetValueNames();
+                for (int j = 0; j < valueNames.Length; j++)
+                {
+                    object value = version.GetValue(valueNames[j]);
+                    if (value == null)
+                    {
+                        System.Console.WriteLine(valueNames[j] + ": null");
+                    }
+                    else
+                    {
+                        System.Console.WriteLine(valueNames[j] + ": " + value.ToString());
+                    }
+                }
+
+                System.Console.WriteLine();
+            }
+
+            System.Console.ReadLine();
+
+            //return display_framwork_name;
         }
     }
 }
